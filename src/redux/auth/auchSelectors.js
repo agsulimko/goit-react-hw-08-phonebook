53f770db61490af4ab5368ab21e60199460bd00e
agsulimko <125898308+agsulimko@users.noreply.github.com>
2023-11-26 22:47:00 +0100

    s

--- src/redux/auth/auchSelectors.js
+++ src/redux/auth/auchSelectors.js
@@ -1,3 +1,3 @@
 export const selectAuth = state => state.auth.token;
-export const selectUser = state => state.auth.profile;
+export const selectUser = state => state.auth.user;
 // export const selectProfile = state => state.auth.user;
